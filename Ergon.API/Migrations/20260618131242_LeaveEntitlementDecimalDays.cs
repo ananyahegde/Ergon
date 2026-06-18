4385f4b3987f8c94b3bff1f3b2c95e2722e09286
ananyahegde <ananyahegde533@gmail.com>
2026-06-18 20:02:19 +0530

    make Days field decimal in LeaveEntitlementComponent.cs

---
+++ Ergon.API/Migrations/20260618131242_LeaveEntitlementDecimalDays.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ergon.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class LeaveEntitlementDecimalDays : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalDays",
+                table: "LeaveEntitlementComponents",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 1,
+                column: "TotalDays",
+                value: 12m);
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 2,
+                column: "TotalDays",
+                value: 6m);
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 3,
+                column: "TotalDays",
+                value: 15m);
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 4,
+                column: "TotalDays",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 5,
+                column: "TotalDays",
+                value: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalDays",
+                table: "LeaveEntitlementComponents",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 1,
+                column: "TotalDays",
+                value: 12);
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 2,
+                column: "TotalDays",
+                value: 6);
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 3,
+                column: "TotalDays",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 4,
+                column: "TotalDays",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "LeaveEntitlementComponents",
+                keyColumn: "LeaveEntitlementComponentId",
+                keyValue: 5,
+                column: "TotalDays",
+                value: 0);
+        }
+    }
+}
--- Ergon.API/Models/LeaveEntitlementComponent.cs
+++ Ergon.API/Models/LeaveEntitlementComponent.cs
@@ -3,13 +3,11 @@ namespace Ergon.Models
     public class LeaveEntitlementComponent
     {
         public int LeaveEntitlementComponentId { get; set; }
-        public int TotalDays { get; set; }
+        public decimal TotalDays { get; set; }
 
-        // foreign keys
         public int LeaveEntitlementId { get; set; }
         public int LeaveTypeId { get; set; }
 
-        // navigation
         public LeaveEntitlement LeaveEntitlement { get; set; } = null!;
         public LeaveType LeaveType { get; set; } = null!;
     }
